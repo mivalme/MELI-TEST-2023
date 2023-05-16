84a0e9b04346525abb9100523673bcb59c806981
Miguel Valcarcel <mivalme@gmail.com>
2023-05-16 09:54:28 -0700

    Project Creation

---
+++ Meli Test/Meli Test/Meli_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meli_TestApp.swift
+//  Meli Test
+//
+//  Created by Miguel Valcárcel on 16/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Meli_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
